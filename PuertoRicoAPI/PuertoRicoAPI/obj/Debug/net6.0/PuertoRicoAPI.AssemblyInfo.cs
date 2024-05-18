cf45e1edf2d71bba529dc8a140955b8133750f78
arkadiy akimov <akimov.arkadiy@gmail.com>
2024-05-18 07:56:19 +0300

    fixed quarry discount on builder skip

--- PuertoRicoAPI/PuertoRicoAPI/obj/Debug/net6.0/PuertoRicoAPI.AssemblyInfo.cs
+++ PuertoRicoAPI/PuertoRicoAPI/obj/Debug/net6.0/PuertoRicoAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PuertoRicoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbf006031fedcacfb069951712041c9136f691b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b322f2e426d3da4164e357ee58e2bfebead02b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PuertoRicoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PuertoRicoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
